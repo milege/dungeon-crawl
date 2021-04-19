de61e572640c32331c5d771fe4d0f8c2ffc242df
ntngy <anitanagy1989@gmail.com>
2021-04-19 16:36:29 +0200

    Feat: add new Item class and Key, Sword subclasses

---
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/Key.java
@@ -0,0 +1,13 @@
+package com.codecool.dungeoncrawl.logic.items;
+
+import com.codecool.dungeoncrawl.logic.Cell;
+
+public class Key extends Item {
+    public Key(Cell cell) {
+        super(cell);
+    }
+
+    public String getTileName() {
+        return "key";
+    }
+}
