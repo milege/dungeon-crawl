8247302837dedade20d4d608ae405cda0639a295
Gergely <milegergely@gmail.com>
2021-04-20 11:34:31 +0200

    feat: add inventory methods

--- src/main/java/com/codecool/dungeoncrawl/logic/items/Inventory.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/Inventory.java
@@ -17,8 +17,26 @@ public class Inventory {
         StringBuilder contents = new StringBuilder();
         contents.append("Inventory:\n");
         for (Item item : items) {
-            contents.append(String.format("%s (%s)\n", item.getName(), item.getType()));
+            contents.append(String.format("  %s (%s)\n", item.getName(), item.getType()));
         }
         return String.valueOf(contents);
+    }
+
+    public boolean isInInventory(String itemName){
+        for (Item item : items){
+            if (item.getName().equals(itemName)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void removeItem(String itemName){
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).getName().equals(itemName)){
+                items.remove(i);
+                break;
+            }
+        }
     }
 }
