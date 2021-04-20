c1ada36f42a417b817870a96e8da75cfc3d8be92
Gergely <milegergely@gmail.com>
2021-04-20 09:39:25 +0200

    feat: add Inventory class

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
@@ -1,9 +1,12 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.items.Inventory;
 import com.codecool.dungeoncrawl.logic.items.Item;
 
 public class Player extends Actor {
+    private Inventory inventory = new Inventory();
+
     public Player(Cell cell) {
         super(cell);
     }
---
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/Inventory.java
@@ -0,0 +1,12 @@
+package com.codecool.dungeoncrawl.logic.items;
+
+import java.util.ArrayList;
+
+public class Inventory {
+    private ArrayList<Item> items;
+
+    public Inventory() {
+        this.items = new ArrayList<Item>();
+    }
+
+}
