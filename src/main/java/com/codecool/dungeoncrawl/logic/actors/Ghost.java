575a5228a00452dce376b3d4f37dbdee5f24c3cc
ntngy <anitanagy1989@gmail.com>
2021-04-20 11:02:16 +0200

    Feat: update Ghost class - instances can move go into the wall

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Ghost.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Ghost.java
@@ -10,4 +10,15 @@ public class Ghost extends Actor {
     @Override
     public String getTileName() { return "ghost"; }
 
+    @Override
+    public void move(int dx, int dy) {
+        Cell nextCell = getCell().getNeighbor(dx, dy);
+
+        if((nextCell.getTileName().equals("floor") || nextCell.getTileName().equals("wall")) && nextCell.getActor() == null){
+            getCell().setActor(null);
+            nextCell.setActor(this);
+            setCell(nextCell);
+        }
+    }
+
 }
