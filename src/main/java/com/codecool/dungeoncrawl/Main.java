efa63b7ace1adacb7459d1f7ae41a002ccfcb45a
mtamas <gpdcaribbeansea@gmail.com>
2021-04-21 11:17:03 +0200

    feat: player can enter name, player with developers' name can walk through walls

--- src/main/java/com/codecool/dungeoncrawl/Main.java
+++ src/main/java/com/codecool/dungeoncrawl/Main.java
@@ -11,9 +11,11 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -24,11 +26,14 @@ public class Main extends Application {
             map.getWidth() * Tiles.TILE_WIDTH,
             map.getHeight() * Tiles.TILE_WIDTH);
     GraphicsContext context = canvas.getGraphicsContext2D();
+    TextField nameField = new TextField("Your name");
+    Label nameLabel = new Label();
     Label healthLabel = new Label();
     Label attackLabel = new Label();
     Label defenseLabel = new Label();
     Label inventoryLabel = new Label();
     Button itemPickUpButton = new Button("Pick up item");
+    Button nameSubmitButton = new Button("+");
 
     public static void main(String[] args) {
         launch(args);
@@ -36,22 +41,34 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        nameField.setPrefWidth(8);
         GridPane ui = new GridPane();
         ui.setPrefWidth(200);
         ui.setPadding(new Insets(10));
 
-        ui.add(new Label("Health: "), 0, 0);
-        ui.add(healthLabel, 1, 0);
-        ui.add(new Label("Attack: "), 0, 1);
-        ui.add(attackLabel, 1, 1);
-        ui.add(new Label("Defense: "), 0, 2);
-        ui.add(defenseLabel, 1, 2);
-        ui.add(inventoryLabel, 0, 3);
-        ui.add(itemPickUpButton,0,4);
+        ui.add(new Label("Enter your name: "), 0, 0);
+        ui.add(nameField, 0, 1);
+        ui.add(nameSubmitButton,1,1);
+        ui.add(new Label("Name: "), 0, 3);
+        ui.add(nameLabel, 1, 3);
+        ui.add(new Label("Health: "), 0, 4);
+        ui.add(healthLabel, 1, 4);
+        ui.add(new Label("Attack: "), 0, 5);
+        ui.add(attackLabel, 1, 5);
+        ui.add(new Label("Defense: "), 0, 6);
+        ui.add(defenseLabel, 1, 6);
+        ui.add(inventoryLabel, 0, 7);
+        ui.add(itemPickUpButton,0,8);
 
 
         itemPickUpButton.setOnAction(onClick -> {
             map.getPlayer().pickUpItem();
+            ui.requestFocus();
+            refresh();
+        });
+
+        nameSubmitButton.setOnAction(onClick -> {
+            map.getPlayer().setName(nameField.getText());
             ui.requestFocus();
             refresh();
         });
@@ -111,6 +128,7 @@ public class Main extends Application {
                 }
             }
         }
+        nameLabel.setText("" + nameField.getText());
         healthLabel.setText("" + map.getPlayer().getHealth());
         attackLabel.setText("" + map.getPlayer().getAttackStrength());
         defenseLabel.setText("" + map.getPlayer().getDefenseStrength());
