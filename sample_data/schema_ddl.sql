374246ccc9b0c0b9c36532deb843cde4bd33a9e0
ntngy <anitanagy1989@gmail.com>
2021-05-03 14:36:13 +0200

    feat: add SQL schema

--- sample_data/schema_ddl.sql
+++ sample_data/schema_ddl.sql
@@ -12,8 +12,45 @@ CREATE TABLE public.player (
     player_name text NOT NULL,
     hp integer NOT NULL,
     x integer NOT NULL,
-    y integer NOT NULL
+    y integer NOT NULL, 
+    defense integer NOT NULL,
+    attack integer NOT NULL
+);
+
+DROP TABLE IF EXISTS public.inventory;
+CREATE TABLE public.inventory (
+    id serial NOT NULL PRIMARY KEY,
+    item_name text NOT NULL,
+    player_id integer NOT NULL
+);
+
+DROP TABLE IF EXISTS public.monsters;
+CREATE TABLE public.monsters (
+    id serial NOT NULL PRIMARY KEY,
+    monster_name text NOT NULL,
+    x integer NOT NULL,
+    y integer NOT NULL,
+    game_id integer NOT NULL
+);
+
+DROP TABLE IF EXISTS public.items;
+CREATE TABLE public.items (
+    id serial NOT NULL PRIMARY KEY,
+    item_name text NOT NULL,
+    x integer NOT NULL,
+    y integer NOT NULL,
+    game_id integer NOT NULL
 );
 
 ALTER TABLE ONLY public.game_state
     ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id);
+
+ALTER TABLE ONLY public.inventory
+    ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id);
+    
+ALTER TABLE ONLY public.monsters
+    ADD CONSTRAINT fk_game_id FOREIGN KEY (game_id) REFERENCES public.game_state(id);
+
+ALTER TABLE ONLY public.items
+    ADD CONSTRAINT fk_game_id FOREIGN KEY (game_id) REFERENCES public.game_state(id);
+    
