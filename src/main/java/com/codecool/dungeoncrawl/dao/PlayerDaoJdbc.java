d7be33a68046ce9b469ef9da0c53cfe2b2f3547a
Gergely <milegergely@gmail.com>
2021-05-04 09:44:29 +0200

    feat: add save button, player saving to database

--- src/main/java/com/codecool/dungeoncrawl/dao/PlayerDaoJdbc.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/PlayerDaoJdbc.java
@@ -16,12 +16,14 @@ public class PlayerDaoJdbc implements PlayerDao {
     @Override
     public void add(PlayerModel player) {
         try (Connection conn = dataSource.getConnection()) {
-            String sql = "INSERT INTO player (player_name, hp, x, y) VALUES (?, ?, ?, ?)";
+            String sql = "INSERT INTO player (player_name, hp, x, y, defense, attack) VALUES (?, ?, ?, ?, ?, ?)";
             PreparedStatement statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             statement.setString(1, player.getPlayerName());
             statement.setInt(2, player.getHp());
             statement.setInt(3, player.getX());
             statement.setInt(4, player.getY());
+            statement.setInt(5, player.getDefense());
+            statement.setInt(6, player.getAttack());
             statement.executeUpdate();
             ResultSet resultSet = statement.getGeneratedKeys();
             resultSet.next();
--- src/main/java/com/codecool/dungeoncrawl/model/PlayerModel.java
+++ src/main/java/com/codecool/dungeoncrawl/model/PlayerModel.java
@@ -7,6 +7,24 @@ public class PlayerModel extends BaseModel {
     private int hp;
     private int x;
     private int y;
+    private int defense;
+    private int attack;
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
 
     public PlayerModel(String playerName, int x, int y) {
         this.playerName = playerName;
@@ -18,8 +36,9 @@ public class PlayerModel extends BaseModel {
         this.playerName = player.getName();
         this.x = player.getX();
         this.y = player.getY();
-
         this.hp = player.getHealth();
+        this.defense = player.getDefenseStrength();
+        this.attack = player.getAttackStrength();
 
     }
 
