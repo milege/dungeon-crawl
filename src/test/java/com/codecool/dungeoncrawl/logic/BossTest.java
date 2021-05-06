c83642af0a7e442c8d7910660946c02648970a35
ntngy <anitanagy1989@gmail.com>
2021-05-06 11:42:39 +0200

    test: add DevilDoesntMove method in BossTest

--- src/test/java/com/codecool/dungeoncrawl/logic/BossTest.java
+++ src/test/java/com/codecool/dungeoncrawl/logic/BossTest.java
@@ -1,5 +1,6 @@
 package com.codecool.dungeoncrawl.logic;
 
+import com.codecool.dungeoncrawl.logic.actors.Devil;
 import com.codecool.dungeoncrawl.logic.actors.Faceless;
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +13,14 @@ public class BossTest {
     void FacelessDoesntMove() {
         Faceless faceless = new Faceless(gameMap.getCell(2, 1));
         faceless.move(1, 0);
+
+        assertNull(gameMap.getCell(3, 1).getActor());
+    }
+
+    @Test
+    void DevilDoesntMove() {
+        Devil devil = new Devil(gameMap.getCell(2, 1));
+        devil.move(1, 0);
 
         assertNull(gameMap.getCell(3, 1).getActor());
     }
